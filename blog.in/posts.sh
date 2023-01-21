4d915979e077f0206a7adf1d4e20132244bb19c4
Benjamin Leskey <ben@benleskey.com>
2023-01-21 15:58:43 -0500

    Mapping tool 0.5 release post

--- blog.in/posts.sh
+++ blog.in/posts.sh
@@ -1,3 +1,4 @@
+post mapper0_5 "Mapping tool 0.5 release" 2023-01-21 "mappingtool software"
 post website_build_script "benleskey.com build script" 2023-01-12 "software website"
 post mapper0_4 "Mapping tool 0.4 release" 2023-01-04 "mappingtool software"
 post mapper0_3 "Mapping tool 0.3 release" 2022-10-26 "mappingtool software"
