9ddd941f00734b38bfb80097308bb40fe4346e58
Benjamin Leskey <ben@benleskey.com>
2023-01-27 13:21:38 -0500

    Use tmp dir for temporary files

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+tmp="$(mktemp -d)"
+
+finish() {
+	echo "Done, removing $tmp"
+	rm -r "$tmp"
+}
+
+trap finish EXIT
+
 # Akas are permanent names that redirect to various resources. If the location of the resource changes, the aka can simply be updated.
 # Due to being hosted on Github pages we do not have direct control over redirects, so we must use meta refresh directives.
 echo "Akas..."
@@ -74,6 +83,7 @@ commonreplace() {
 }
 
 mkdir -p blog/tags
+mkdir -p "$tmp"/tags
 
 # The list of all tags, to be built up as posts are processed.
 alltags=""
@@ -90,8 +100,8 @@ BN=0
 echo "Blog..."
 
 # Begin the major and minor blog post lists.
-echo "<ul>" > blog/_major.in.html
-echo "<ul>" > blog/_minor.in.html
+echo "<ul>" > "$tmp"/_major.in.html
+echo "<ul>" > "$tmp"/_minor.in.html
 
 # The post function is called from the posts.sh list of posts; it is responsible for processing each blog post.
 # A post has four components, reflected in the arguments:
@@ -105,7 +115,7 @@ post() {
 	# We'll be reading the text of the blog post from an input HTML file and generating the full HTML page.
 	infile="blog.in/$name.in.html"
 	outfile="blog/$name.html"
-	outfeed="blog/_$name.xml"
+	outfeed="$tmp/_$name.xml"
 
 	echo " $name: $title [$date]"
 
@@ -125,8 +135,6 @@ post() {
 		if ! echo "$alltags" | grep -w "$tag" > /dev/null; then
 			echo " New tag: $tag"
 			alltags="$alltags $tag"
-			rm -f "blog/_tag_$tag.html"
-			rm -f "blog/_tag_feed_$tag.xml"
 		fi
 
 	done < <(echo "$tags" | xargs -n1 --no-run-if-empty)
@@ -134,7 +142,7 @@ post() {
 	# Now we will write a link to this post to the each of its tags' pages.
 	# We do this after tag processing because a link to the post also includes its tags, so we need to know all the tags of the post.
 	while read tag; do
-		echo "<li class='postlisting'><a href='__ROOT__/blog/$name'>$title</a> [$date] $smalltaghtml</li>" >> "blog/_tag_$tag.html"
+		echo "<li class='postlisting'><a href='__ROOT__/blog/$name'>$title</a> [$date] $smalltaghtml</li>" >> "$tmp/_tag_$tag.html"
 	done < <(echo "$tags" | xargs -n1 --no-run-if-empty)
 
 	# Record that another post was processed.
@@ -142,11 +150,11 @@ post() {
 
 	# If we haven't reached the limit for "minor" posts, add a link to the minor blog post listing HTML, this is for the "recent posts" section of the main page.
 	if [ $BN -le $BMMAX ]; then
-		echo "<li class='postlisting'><a href='__ROOT__/blog/$name'>$title</a> [$date] $minortaghtml</li>" >> blog/_minor.in.html
+		echo "<li class='postlisting'><a href='__ROOT__/blog/$name'>$title</a> [$date] $minortaghtml</li>" >> "$tmp"/_minor.in.html
 	fi
 
 	# Always add a link to the "major" blog post listing HTML, this is for the dedicated blog page.
-	echo "<li class='postlisting'><a href='__ROOT__/blog/$name'>$title</a> [$date] $smalltaghtml</li>" >> blog/_major.in.html
+	echo "<li class='postlisting'><a href='__ROOT__/blog/$name'>$title</a> [$date] $smalltaghtml</li>" >> "$tmp"/_major.in.html
 
 	# Build the blog post HTML.
 	(commonreplace .. | replacetext "__TITLE__" "$title" | replacetext "__POST_DATE__" "$date" | replacetext "__TAGS__" "$taghtml" | replacefile "__POST__" "$infile") < templates/post.in.html > "$outfile" &
@@ -164,28 +172,28 @@ source blog.in/posts.sh
 
 # If there are more posts than can be displayed in the minor list (on the main page) then add a "More..." link to the minor list.
 if [ $BN -gt $BMMAX ]; then
-	echo "<li><a href='blog'>$((BN - BMMAX)) more...</a></li>" >> blog/_minor.in.html
+	echo "<li><a href='blog'>$((BN - BMMAX)) more...</a></li>" >> "$tmp"/_minor.in.html
 fi
 
 # Cap off the lists.
-echo "</ul>" >> blog/_minor.in.html
-echo "</ul>" >> blog/_major.in.html
+echo "</ul>" >> "$tmp"/_minor.in.html
+echo "</ul>" >> "$tmp"/_major.in.html
 
 echo " Waiting..."
 wait
 
-echo "" > blog/_feed.in.xml
+echo "" > "$tmp"/_feed.in.xml
 
 # Concat all the RSS items
 post() {
 	name="$1"
 	tags="$4"
 
-	feedfile="blog/_$name.xml"
-	cat < "$feedfile" >> blog/_feed.in.xml
+	feedfile="$tmp/_$name.xml"
+	cat < "$feedfile" >> "$tmp"/_feed.in.xml
 
 	while read tag; do
-		cat < "$feedfile" >> blog/_tag_feed_"$tag".xml
+		cat < "$feedfile" >> "$tmp"/_tag_feed_"$tag".xml
 	done < <(echo "$tags" | xargs -n1 --no-run-if-empty)
 
 }
@@ -194,40 +202,40 @@ source blog.in/posts.sh
 
 # Build the main page.
 (
-	replacefile "__BLOGMINOR__" "blog/_minor.in.html" |
+	replacefile "__BLOGMINOR__" "$tmp/_minor.in.html" |
 	commonreplace .
 ) < templates/index.in.html > index.html
 
 # Build the dedicated blog index page.
-(replacefile "__BLOGMAJOR__" "blog/_major.in.html" | replacetext __NUM_POSTS__ "$BN" | commonreplace ..) < templates/blog.in.html > blog/index.html
+(replacefile "__BLOGMAJOR__" "$tmp/_major.in.html" | replacetext __NUM_POSTS__ "$BN" | commonreplace ..) < templates/blog.in.html > blog/index.html
 
 
 # Uniquify and sort the list of all tags.
 alltags="$(echo "$alltags" | xargs -n1 | sort | uniq | xargs)"
 
 # The tag index page consist of all the tags with every post categorized under their tag/tags.
-echo > blog/_tags.in.html
+echo > "$tmp"/_tags.in.html
 
 # For each tag, just append the previously generated list of posts HTML for that tag.
 while read tag; do
 	echo " Tag: $tag"
-	echo "<h2 id='$tag'><a href='__ROOT__/blog/tags/$tag'>$tag</a></h2>" >> blog/_tags.in.html
-	echo "<ul>" >> blog/_tags.in.html
-	cat "blog/_tag_$tag.html" >> blog/_tags.in.html
-	echo "</ul>" >> blog/_tags.in.html
+	echo "<h2 id='$tag'><a href='__ROOT__/blog/tags/$tag'>$tag</a></h2>" >> "$tmp"/_tags.in.html
+	echo "<ul>" >> "$tmp"/_tags.in.html
+	cat "$tmp/_tag_$tag.html" >> "$tmp"/_tags.in.html
+	echo "</ul>" >> "$tmp"/_tags.in.html
 
-	(replacefile "__ITEMS__" "blog/_tag_$tag.html" | replacetext __NUM_POSTS__ "$(wc -l "blog/_tag_$tag.html" | cut -d' ' -f1)" | replacetext __TAG__ "$tag" | commonreplace ../..) < templates/tag.in.html > blog/tags/"$tag".html &
-	(replacefile "__FEED_" "blog/_tag_feed_$tag.xml" | replacetext __TAG__ "$tag" | commonreplace ../..) < templates/tag_feed.in.xml > blog/tags/"$tag.feed.xml" &
+	(replacefile "__ITEMS__" "$tmp/_tag_$tag.html" | replacetext __NUM_POSTS__ "$(wc -l "$tmp/_tag_$tag.html" | cut -d' ' -f1)" | replacetext __TAG__ "$tag" | commonreplace ../..) < templates/tag.in.html > blog/tags/"$tag".html &
+	(replacefile "__FEED_" "$tmp/_tag_feed_$tag.xml" | replacetext __TAG__ "$tag" | commonreplace ../..) < templates/tag_feed.in.xml > blog/tags/"$tag.feed.xml" &
 done < <(echo "$alltags" | xargs -n1 --no-run-if-empty)
 
 echo " Waiting..."
 wait
 
 # Generate the full tag index page.
-(replacefile "__TAGS__" "blog/_tags.in.html" | replacetext __NUM_TAGS__ "$(echo "$alltags" | wc -w)" | commonreplace ../..) < templates/tags.in.html > blog/tags/index.html
+(replacefile "__TAGS__" "$tmp/_tags.in.html" | replacetext __NUM_TAGS__ "$(echo "$alltags" | wc -w)" | commonreplace ../..) < templates/tags.in.html > blog/tags/index.html
 
 echo "Generating RSS feed..."
-(replacefile "__FEED__" "blog/_feed.in.xml" | commonreplace ..) < templates/feed.in.xml > blog/feed.xml
+(replacefile "__FEED__" "$tmp/_feed.in.xml" | commonreplace ..) < templates/feed.in.xml > blog/feed.xml
 
 echo "Downloading images..."
 while read line; do
