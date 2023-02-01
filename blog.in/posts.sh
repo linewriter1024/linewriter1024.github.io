29e746c0731e298d7eb63c73b9b1fed1461c94ab
Benjamin Leskey <ben@benleskey.com>
2023-01-31 21:21:27 -0500

    Finish raymon 2

--- blog.in/posts.sh
+++ blog.in/posts.sh
@@ -1,4 +1,4 @@
-POST_SERIES=cdda_raymon post cdda_raymon_2 "Raymon Ortega #2: Battery Heist" 2023-01-31 "games letsplay cdda cdda_raymon"
+POST_SERIES=cdda_raymon post cdda_raymon_2 "Raymon Ortega #2: Battery Heist and a Sweet Chariot" 2023-01-31 "games letsplay cdda cdda_raymon"
 POST_SERIES=cdda_raymon post cdda_raymon_1 "Raymon Ortega #1: Would-be Wizard" 2023-01-29 "games letsplay cdda cdda_raymon"
 post more_rss_feeds "More RSS feeds!" 2023-01-27 "software website"
 post test_post "Test post, please ignore" 2023-01-27 ""
