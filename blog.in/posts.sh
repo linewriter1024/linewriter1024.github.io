d8e2f83881a1626521ba0bc8cc18d2ef2b999c57
Benjamin Leskey <ben@benleskey.com>
2023-02-08 02:00:46 -0500

    CDDA autodrive into a crater fix

--- blog.in/posts.sh
+++ blog.in/posts.sh
@@ -1,3 +1,4 @@
+post cdda_no_hole "Autodrive into a crater fix" 2023-02-08 "cdda software"
 POST_SERIES=cdda_erik post cdda_erik_3 "Erik Otto #3: Preparing for battle" 2023-02-06 "games letsplay cdda cdda_erik"
 POST_SERIES=cdda_erik post cdda_erik_2 "Erik Otto #2: Base building and car collecting" 2023-02-06 "games letsplay cdda cdda_erik"
 POST_SERIES=cdda_erik post cdda_erik_1 "Erik Otto #1: Used car cult salesman" 2023-02-05 "games letsplay cdda cdda_erik"
