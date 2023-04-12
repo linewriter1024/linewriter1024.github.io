49ae87df5661a446aa9b48c0f5f2286f6e1fac6e
linewriter1024 <linewriter1024@users.noreply.github.com>
2023-04-12 16:22:45 +0000

    deploy: b260d40d86117463654717a69ee713960e3cd34f

--- build.sh
+++ build.sh
@@ -241,7 +241,8 @@ source blog.in/posts.sh
 # Build the main page.
 (
 	replacefile "__BLOGMINOR__" "$tmp/_minor.in.html" |
-	commonreplace .
+	commonreplace . |
+	python3 element_copy.py
 ) < templates/index.in.html > index.html
 
 # Uniquify and sort the list of all tags.
