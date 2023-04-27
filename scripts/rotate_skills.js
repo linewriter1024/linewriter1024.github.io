54a39228ab7b09ee8f7e32adf6bfba991c65e78e
linewriter1024 <linewriter1024@users.noreply.github.com>
2023-04-27 00:44:38 +0000

    deploy: 501f485c341ef0f57923157a910abf3652bf5f87

--- scripts/rotate_skills.js
+++ scripts/rotate_skills.js
@@ -1,4 +1,6 @@
 {
+	const skillsToggle = document.getElementById("skills-toggle");
+
 	const skills = document.querySelector("#skillslist");
 	const skillNodes = Array.from(skills.children);
 
@@ -21,43 +23,87 @@
 		maxHeight = Math.max(maxHeight, skill.offsetHeight);
 	}
 
-	skills.style.height = `${maxHeight * 1.1}px`;
+	skills.style.minHeight = `${maxHeight * 1.1}px`;
 
-	skills.innerHTML = "";
+	let allSkills = false;
 
-	function rotate() {
-		const choices = Array.from(skillNodes);
-		const got = [];
-		let width = 0;
-		let tries = 25;
-		while(got.length < 10 && tries > 0) {
-			tries--;
-			const index = Math.floor(Math.random() * choices.length);
-			const choice = choices[index];
-			const choiceWidth = measureSkill(choice).width * 1.1;
-			if(width + choiceWidth > skills.offsetWidth) {
-				continue;
-			}
-			width += choiceWidth;
-			got.push(choice);
-			choices.splice(index, 1);
+	skillsToggle.addEventListener("click", function() {
+		allSkills = !allSkills;
+		reset();
+	});
+
+	window.addEventListener("resize", function() {
+		reset();
+	})
+
+	let intervalId;
+	let timeoutId;
+
+	function reset() {
+		if(intervalId) {
+			clearInterval(intervalId);
 		}
 
-		for(const skill of skills.children) {
-			skill.classList.remove("grow");
-			skill.classList.add("shrink");
+		if(timeoutId) {
+			clearTimeout(timeoutId);
 		}
 
-		setTimeout(function() {
-			skills.innerHTML = "";
-			for(const skill of got) {
-				const newSkill = skill.cloneNode(true)
-				newSkill.classList.add("grow");
-				skills.appendChild(newSkill);
+		skills.innerHTML = "";
+
+		if(allSkills) {
+			const choices = Array.from(skillNodes);
+			while(choices.length > 0) {
+				const index = Math.floor(Math.random() * choices.length);
+				const choice = choices[index];
+				skills.appendChild(choice.cloneNode(true));
+				choices.splice(index, 1);
 			}
-		}, 1000);
+		}
+		else {
+			function rotate() {
+				const choices = Array.from(skillNodes);
+				const got = [];
+				let width = 0;
+				let tries = 25;
+				while(got.length < 10 && tries > 0) {
+					tries--;
+					const index = Math.floor(Math.random() * choices.length);
+					const choice = choices[index];
+					const choiceWidth = measureSkill(choice).width * 1.1;
+					if(width + choiceWidth > skills.offsetWidth) {
+						continue;
+					}
+					width += choiceWidth;
+					got.push(choice);
+					choices.splice(index, 1);
+				}
+
+				for(const skill of skills.children) {
+					skill.classList.remove("grow");
+					skill.classList.add("shrink");
+				}
+
+				function grow() {
+					skills.innerHTML = "";
+					for(const skill of got) {
+						const newSkill = skill.cloneNode(true)
+						newSkill.classList.add("grow");
+						skills.appendChild(newSkill);
+					}
+				}
+
+				if(skills.children.length > 0) {
+					timeoutId = setTimeout(grow, 1000);
+				}
+				else {
+					grow();
+				}
+			}
+
+			intervalId = setInterval(rotate, 4000);
+			rotate();
+		}
 	}
 
-	setInterval(rotate, 4000);
-	rotate();
+	reset();
 }
