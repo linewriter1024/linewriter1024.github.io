306b5973f6185d4a2f54196d4a2f6c64639b2143
Benjamin Leskey <ben@benleskey.com>
2023-09-28 13:52:18 -0400

    Add tpt blog post

--- blog.in/posts.sh
+++ blog.in/posts.sh
@@ -1,3 +1,4 @@
+post tpt_world_map "World map for a Wesnoth campaign: The Paradise Trap" 2023-09-28 "software games wesnoth"
 post dcss_sh "The new DCSS shapeshifter background (first win)" 2023-07-28 "games dcss"
 POST_EXTERNAL="https://github.com/CleverRaven/Cataclysm-DDA/pull/66639" post make_horses_milkable "Make horses milkable" 2023-07-03 "cdda software"
 POST_BLURB="Culture and mystery!" POST_INTERNAL="reading_list#2023-06-06" post reading_list_2 "Reading list update #2" 2023-06-06 "reading discovery"
