77fe9b2a6dc4924fdbe2c17c9f2afe9e001b9b81
Benjamin Leskey <ben@benleskey.com>
2023-10-20 21:04:56 -0400

    Arcana technoclysm

--- blog.in/posts.sh
+++ blog.in/posts.sh
@@ -1,3 +1,4 @@
+post cdda_arcana_technoclysm "Arcana Technoclysm: A small mod for Cataclysm: DDA" 2023-10-20 "software games cdda"
 POST_BLURB="Sci-fi, history, Mark Twain!" POST_INTERNAL="reading_list#2023-10-06" post reading_list_3 "Reading list update #3" 2023-10-06 "reading discovery"
 post tpt_world_map "World map for a Wesnoth campaign: The Paradise Trap" 2023-09-28 "software games wesnoth"
 post dcss_sh "The new DCSS shapeshifter background (first win)" 2023-07-28 "games dcss"
