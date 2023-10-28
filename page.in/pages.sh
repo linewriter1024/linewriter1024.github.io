09c932ba303e7ce1b3685cd9c20f4832d228e294
linewriter1024 <linewriter1024@users.noreply.github.com>
2023-10-28 14:15:25 +0000

    deploy: 949e3ae090dd9478be97691173353b3e89b6bf17

--- page.in/pages.sh
+++ page.in/pages.sh
@@ -1,2 +1,2 @@
-page mc_bedrock_how_connect "How to connect to a Minecraft Bedrock server" 2023-06-06
 page reading_list "Recommended reading list" 2023-10-06
+page mc_bedrock_how_connect "How to connect to a Minecraft Bedrock server" 2023-06-06
