6219280e322a3aec9d251aa3ebfe3ca4c0d0307e
Benjamin Leskey <ben@benleskey.com>
2024-01-15 10:55:01 -0500

    Add amethyst farm post

--- blog.in/posts.sh
+++ blog.in/posts.sh
@@ -1,3 +1,4 @@
+post minecraft_amethyst_farm "First Version of an Amethyst Farm" 2024-01-14 "games minecraft"
 post cdda_improvements "Recent great improvements in Cataclysm: DDA" 2023-11-02 "games cdda"
 post cdda_arcana_technoclysm "Arcana Technoclysm: A small mod for Cataclysm: DDA" 2023-10-20 "software games cdda"
 POST_BLURB="Sci-fi, history, Mark Twain!" POST_INTERNAL="reading_list#2023-10-06" post reading_list_3 "Reading list update #3" 2023-10-06 "reading discovery"
