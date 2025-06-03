b20c38a2fcf8b9c93727b7609f53b802d0828e32
linewriter1024 <linewriter1024@users.noreply.github.com>
2025-06-03 02:00:38 +0000

    deploy: 4b676ab8212d4394d8449e85dfadca91f062ed4b

--- blog.in/posts.sh
+++ blog.in/posts.sh
@@ -1,3 +1,4 @@
+POST_BLURB="A collection of random tables" POST_INTERNAL="dndtables" post dndtables "D&D random tables" 2025-06-01 "games dnd"
 post intellj_kde_lag_spike_workaround "Fixing Intellj IDEA lag spikes on OpenSUSE KDE Plasma 6" 2025-04-17 "debugging software"
 post dcss_gods_surface "DCSS deities on the surface" 2025-03-20 "dcss games dnd"
 post opensuse_mic "Fixing HP Victus 15 internal microphone drivers by compiling patched auto-updating kernel with openSUSE Tumbleweed Open Build Service" 2024-12-25 "debugging software"
--- page.in/pages.sh
+++ page.in/pages.sh
@@ -1,2 +1,3 @@
+page dndtables "D&D tables: Random generation tables for RPG games" 2025-06-01
 page reading_list "Recommended reading list" 2024-08-13
 page mc_bedrock_how_connect "How to connect to a Minecraft Bedrock server" 2023-06-06
